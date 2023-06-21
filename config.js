07c1d6e099ba1a0a6ce3582230892a1c726cbd8e
Berlin <berlin@home.com>
2023-06-21 15:41:45 +0800

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports =  {
     token: '6162797200:AAHEW85jl7QbQfsgAX5UFU3dvk4UhurzEIo',
     webhook: {
-        url: 'https://tgbot-phi.vercel.app'
+        url: 'https://tgbot-jahaih6yg-yqmc7124.vercel.app'
     }
 }
