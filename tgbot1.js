b62dc83a11512f58dc9311d3bb435c2037d4f097
yqmc7124 <yqmc7124@gmail.com>
2023-06-23 21:27:08 +0800

    verbal 挂起问题

--- tgbot1.js
+++ tgbot1.js
@@ -1,8 +1,11 @@
 const config = require('./config')
 const TelegramBot = require('./src/telegram');
 
+const msgKey = ['message', 'edited_message', 'channel_post', 'edited_channel_post', 'inline_query', 'chosen_inline_result', 'callback_query', 'shipping_query', 'pre_checkout_query',
+'poll', 'poll_answer', 'chat_member', 'my_chat_member', 'chat_join_request']
 var lastMessage;
 var photoUrl = 'https://telegram.org/img/tl_card_destruct.gif';
+var callback = {}
 
 var bot
 if (config.webhook) {
@@ -25,7 +28,9 @@ bot.onText(/\/start/, msg => {
         lastMessage = [msg.from.id, re.message_id];
        return bot.sendMessage(msg.from.id, 'Now set a new caption using /edit <caption>');
     })
-
+        .finally(() => {
+            execCallback()
+        })
 });
 
 bot.onText(/\/edit/, msg => {
@@ -42,29 +47,50 @@ bot.onText(/\/edit/, msg => {
     return bot.editMessageCaption(caption, {chat_id: chatId, message_id: messageId}).then(() => {
       return bot.sendMessage(msg.from.id, `Caption changed to: ${ caption }`);
     })
+        .finally(() => {
+            execCallback()
+        })
 });
 
 bot.onText(/^[^/].*/, msg => {
    return bot.sendMessage(msg.chat.id, 'I am alive!')
+       .finally(() => {
+            execCallback()
+        })
 })
 
 function handleUpdateMessage(body) {
     console.log("handleUpdateMessage:", body)
     if (body) bot.processUpdate(body);
 }
-
+function addCallback(data, fun) {
+    for (let k of msgKey) {
+        if (data[k]) {
+            data[k].update_id = data.update_id
+            callback[data.update_id] = fun
+        }
+    }
+}
+function execCallback(msg) {
+    if (callback[msg.update_id]) {
+        callback[msg.update_id]()
+        callback[msg.update_id] = undefined
+    }
+}
 module.exports = {
     listener: function (req, res, next) {
-        console.log("url:", req.url)
+        // console.log("url:", req.url)
         switch (req.url) {
             case '/updateMessage': {
                 let body = '';
                 req.on('data', (data) => body += data);
                 req.on('end', () => {
-                    handleUpdateMessage(JSON.parse(body))
-                    setTimeout(() => {
+                    let data = JSON.parse(body)
+                    handleUpdateMessage(data)
+                    //vercel部署后，项目不是持续监听的，而是只有webhook触发时才激活，res.end()后又会立刻挂起，从而导致异步操作无法完成
+                    addCallback(data, () => {
                         res.end()
-                    }, 1000)
+                    })
                 })
                 break
             }
