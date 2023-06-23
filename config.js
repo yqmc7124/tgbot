34b7481dafb65181ad33be1692076c1b74862245
yqmc7124 <yqmc7124@gmail.com>
2023-06-23 13:26:37 +0800

    update

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports =  {
     token: '6162797200:AAHEW85jl7QbQfsgAX5UFU3dvk4UhurzEIo',
-    webhook: {
-        url: 'https://tgbot-seven-eta.vercel.app/api/tg/updateMessage'
-    },
+    // webhook: {
+    //     url: 'https://tgbot-seven-eta.vercel.app/api/tg/updateMessage'
+    // },
     polling: { // Optional. Use polling.
         interval: 1000, // Optional. How often check updates (in ms).
         timeout: 0, // Optional. Update polling timeout (0 - short polling).
         limit: 100, // Optional. Limits the number of updates to be retrieved.
         retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
-        proxy: 'http://127.0.0.1:33210' //本地代理，否则node里的请求不会通过vpn
+        // proxy: 'http://127.0.0.1:33210' //本地代理，否则node里的请求不会通过vpn
     }
 }
