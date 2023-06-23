6c5f7e28b79648124e75e3121e451a595187be3c
yqmc7124 <yqmc7124@gmail.com>
2023-06-23 21:38:03 +0800

    update

--- tgbot1.js
+++ tgbot1.js
@@ -1,5 +1,5 @@
 const config = require('./config')
-const TelegramBot = require('./src/telegram');
+const TelegramBot = require('node-telegram-bot-api');
 
 const msgKey = ['message', 'edited_message', 'channel_post', 'edited_channel_post', 'inline_query', 'chosen_inline_result', 'callback_query', 'shipping_query', 'pre_checkout_query',
 'poll', 'poll_answer', 'chat_member', 'my_chat_member', 'chat_join_request']
